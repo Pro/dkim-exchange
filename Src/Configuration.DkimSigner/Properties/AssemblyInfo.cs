258c201c02ac9fcfe93e3e20887e15c8e2310877
Stefan Profanter <git@s.profanter.me>
2016-07-13 20:46:36 +0200

    v3.0.4

--- Src/Configuration.DkimSigner/Properties/AssemblyInfo.cs
+++ Src/Configuration.DkimSigner/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("github.com/pro")]
 [assembly: AssemblyProduct("Exchange DKIM Signer Configuration")]
-[assembly: AssemblyCopyright("Gnu GPL 3 by Stefan Profanter © 2013")]
+[assembly: AssemblyCopyright("Gnu GPL 3 by Stefan Profanter © 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.0.3")]
-[assembly: AssemblyFileVersion("3.0.3")]
+[assembly: AssemblyVersion("3.0.4")]
+[assembly: AssemblyFileVersion("3.0.4")]
